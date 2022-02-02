d79e6efe72c01851b2f5431e0965e5bec0618c02
KsoftM <90229823+KsoftM@users.noreply.github.com>
2022-02-02 19:04:56 +0530

    name changed

---
+++ src/app/http/middleware/LangMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ksoftm\app\http\middleware;
+
+
+use Closure;
+use ksoftm\system\kernel\Redirect;
+use ksoftm\system\utils\Cookie;
+use ksoftm\system\kernel\Request;
+use ksoftm\system\kernel\Response;
+use ksoftm\system\middleware\MiddlewareFactory;
+
+class LangMiddleware implements MiddlewareFactory
+{
+    public function handle(Request $request, Closure $next): mixed
+    {
+
+        if ($request->exists('lang')) {
+            $cookie = Cookie::make('lang');
+            $request->lang = $cookie->get();
+        } else {
+            $cookie = Cookie::make(
+                'lang',
+                'en',
+                date_create('+1 year')->getTimestamp()
+            )->start(true);
+        }
+
+        return $next($request);
+    }
+}
